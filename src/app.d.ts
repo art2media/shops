4f821d0d08227cec6b5fed31b571fc27cee983c3
Carsten Jaksch <carsten@jaksch.me>
2022-10-30 20:58:57 +0100

    Add Lucia

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,20 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+
+/// <reference types="lucia-auth" />
+declare namespace Lucia {
+	type Auth = import("$lib/server/lucia.js").Auth;
+	type UserAttributes = {};
+}
+
+/// <reference types="@sveltejs/kit" />
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		getSession: import("@lucia-auth/sveltekit").GetSession;
+		setSession: import("@lucia-auth/sveltekit").SetSession;
+		clearSession: import("@lucia-auth/sveltekit").ClearSession;
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
